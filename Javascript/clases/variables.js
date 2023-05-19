ae2ee70cefa07b0c5ab94b479f068273d882d030
HansCris01 <hansluyo@outlook.es>
2023-05-18 19:37:34 -0500

    segundo cambio 2

---
+++ Javascript/clases/variables.js
@@ -0,0 +1,88 @@
+//Vamos usar variables: 
+
+recipiente = "papel";
+
+alert(recipiente) //mostrar lo que tiene variable en un html
+console.log(recipiente); //mostrarlo con NODE.JS usando consola 
+//comando node variables.js
+
+//Tipo de datos: 
+
+string = "texto"
+number = 15
+booleano = true
+
+//Declaraciones de variables var, let, const
+
+//declarar numero: 
+
+var numero = 15; //se uso en el pasado 
+let numero = 18; //se usa ahora 
+
+// varimable constante no va cambiar
+const nombre = "Hans";
+
+//Undifine es cuando no existe valor o esta vacio no hay datos 
+
+//Declarar 3 numeros con let
+
+let numero1, numero2, numero3;
+
+numero1 = 2;
+numero2 = 3; 
+numero3 = 4;
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+
+//declarar 3 variables de otra forma
+
+let numeroA = 23;
+let numeroB = 21;
+let numeroC = 24;
+
+let n1, n2, n3;
+
+n1 = 85;
+n2 = 61;
+n3 = 31;
+
+//declarar variable vacia 
+
+let numero = null;
+
+//suma de numeros:
+let num_1 = 12; 
+let num_2 = 13;
+
+alert(numero + numero);
+
+//otra forma de sumar 
+
+let num_a = 15;
+let num_b = 45;
+
+let suma;
+
+suma = num_a + num_b;
+
+console.log();
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
